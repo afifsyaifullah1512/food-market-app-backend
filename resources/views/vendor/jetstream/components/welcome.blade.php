dbcbc95a48e285ce559dc60a0abebbb0da4a446b
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-07-17 22:17:14 +0700

    update besok lagi

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -2,13 +2,13 @@
     <div class="p-6">
         <div class="flex items-center">
             <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">Foods</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/food">Foods</a></div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">Transaction</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/transaction">Transaction</a></div>
         </div>
     </div>
 
@@ -20,7 +20,7 @@
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#/">Profile</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/user/profile">Profile</a></div>
         </div>
     </div>
 </div>
